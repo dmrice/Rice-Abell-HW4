0c0ed3b880f9b6b681c2e1bf9e45f90b065cd12e
Deanna Rice <dmrice@wpi.edu>
2018-11-26 12:05:48 -0500

    Add exisiting file

---
+++ src/Examples.java
@@ -0,0 +1,60 @@
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+/**
+ * 
+ * @author Deanna Rice and Phillip Abell
+ *
+ */
+public class Examples {
+	public Examples() {}
+	
+	HeapTester h1 = new HeapTester();
+	IHeap emptyHeap = new MtHeap();
+	IHeap fullHeap = new DataHeap(9);
+	IHeap largeHeap = new DataHeap(2, new DataHeap(9), new DataHeap(12));
+	IHeap myHeap = new DataHeap(14);
+	IHeap myBinTree = new DataHeap(5);
+	
+	
+	@Test
+	public void test1(){
+		assertTrue(h1.addEltTester(fullHeap,5,fullHeap.addElt(5)));
+	}
+    @Test
+	 public void test2(){
+	    assertTrue(h1.addEltTester(myHeap,5,myBinTree));
+	  }
+	@Test
+	public void addEmptyElt() {
+		assertTrue(h1.addEltTester(emptyHeap,2,new DataBT(2)));
+	}
+	
+	@Test
+	public void addToFilled() {
+		assertTrue(h1.addEltTester(fullHeap,2,fullHeap.addElt(2)));
+	}
+	
+	@Test
+	public void addToBigList() {
+		assertTrue(h1.addEltTester(largeHeap,5,largeHeap.addElt(5)));
+	}
+	
+	@Test
+	public void removeBecomesEmpty(){
+		assertTrue(h1.remMinEltTester(fullHeap,fullHeap.remMinElt()));
+	}
+	
+	@Test
+	public void removingWithEmpty() {
+		assertTrue(h1.remMinEltTester(emptyHeap,emptyHeap.remMinElt()));
+	}
+	
+	@Test
+	public void removeBigHeap() {
+		assertTrue(h1.remMinEltTester(largeHeap,largeHeap.remMinElt()));
+	}
+
+}
+
---
+++ src/IBinTree.java
@@ -0,0 +1,99 @@
+import java.lang.Math;
+import java.util.LinkedList;
+
+interface IBinTree {
+	//determines whether element is within the tree
+	boolean hasElt(int e);
+	//return how many nodes are in the tree (num), double counts duplicates
+	int size();
+	//return longest branch in the tree
+	int height();
+
+	boolean correctHeap(int previousRoot);
+
+	LinkedList<Integer> toList();
+}
+
+class MtBT implements IBinTree {
+	MtBT(){}
+
+	// returns false since empty tree has no elements
+	public boolean hasElt(int e) {
+		return false;
+	}
+
+	// returns 0 since empty tree has no elements
+	public int size() {
+		return 0;
+	}
+
+	// returns 0 since empty tree has no branches
+	public int height() {
+		return 0;
+	}
+
+	public boolean correctHeap(int previousRoot) {
+		return true;
+	}
+
+	@Override
+	public LinkedList<Integer> toList() {
+		LinkedList<Integer> binTreeList = new LinkedList<Integer>();
+		return binTreeList;
+	}
+}
+class DataBT implements IBinTree {
+	int data;
+	IBinTree left;
+	IBinTree right;
+
+	DataBT(int data, IBinTree left, IBinTree right) {
+		this.data = data;
+		this.left = left;
+		this.right = right;
+	}
+
+	// an alternate constructor for when both subtrees are empty
+	DataBT(int data) {
+		this.data = data;
+		this.left = new MtBT();
+		this.right = new MtBT();
+	}
+
+	// determines whether this node or node in subtree has given element
+	public boolean hasElt(int e) {
+		return this.data == e || this.left.hasElt(e) || this.right.hasElt(e) ;
+	}
+
+	// adds 1 to the number of nodes in the left and right subtrees
+	public int size() {
+		return 1 + this.left.size() + this.right.size();
+	}
+
+	// adds 1 to the height of the taller subtree
+	public int height() {
+		return 1 + Math.max(this.left.height(), this.right.height());
+	}
+
+	//puts all numbers in the binary tree in a list
+	public LinkedList<Integer> toList(){
+		LinkedList<Integer> binTreeList = new LinkedList<Integer>();
+
+		binTreeList.add(this.data);
+		this.left.toList();
+		this.right.toList();
+
+		return binTreeList;
+	}
+
+	public boolean correctHeap(int lastRoot) {
+		if(lastRoot >= this.data) {
+			return false;
+		}
+		else {
+			lastRoot = this.data;
+			return (this.left.correctHeap(lastRoot) && this.right.correctHeap(lastRoot));
+		}
+
+	}
+}
