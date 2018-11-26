9b1dea6443a22317fbef4b61420b89cf588ad7e1
dmrice <42659977+dmrice@users.noreply.github.com>
2018-11-26 16:23:47 -0500

    Add files via upload
    Final Working code atm 4:23 11/26/2018

--- Examples.java
+++ Examples.java
@@ -10,50 +10,49 @@ import org.junit.Test;
 public class Examples {
 	public Examples() {}
 	
-	HeapChecker h1 = new HeapChecker();
+	HeapChecker HT = new HeapChecker();
 	IHeap emptyHeap = new MtHeap();
 	IHeap fullHeap = new DataHeap(9);
 	IHeap largeHeap = new DataHeap(2, new DataHeap(9), new DataHeap(12));
 	IHeap myHeap = new DataHeap(14);
 	IHeap myBinTree = new DataHeap(4, new DataHeap(14), new DataHeap(7));
 	
-	
 	@Test
 	public void test1(){
-		assertTrue(h1.addEltTester(fullHeap,5,fullHeap.addElt(5)));
+		assertTrue(HT.addEltTester(fullHeap,5,fullHeap.addElt(5)));
 	}
     @Test
 	 public void test2(){
-	    assertTrue(h1.addEltTester(myHeap,5,myBinTree));
-	  }
+	    assertTrue(HT.addEltTester(myHeap,5,myBinTree));
+    }
 	@Test
 	public void addEmptyElt() {
-		assertTrue(h1.addEltTester(emptyHeap,2,new DataBT(2)));
+		assertTrue(HT.addEltTester(emptyHeap,2,new DataBT(2)));
 	}
 	
 	@Test
 	public void addToFilled() {
-		assertTrue(h1.addEltTester(fullHeap,2,fullHeap.addElt(2)));
+		assertTrue(HT.addEltTester(fullHeap,2,fullHeap.addElt(2)));
 	}
 	
 	@Test
 	public void addToBigList() {
-		assertTrue(h1.addEltTester(largeHeap,5,largeHeap.addElt(5)));
+		assertTrue(HT.addEltTester(largeHeap,5,largeHeap.addElt(5)));
 	}
 	
 	@Test
 	public void removeBecomesEmpty(){
-		assertTrue(h1.remMinEltTester(fullHeap,fullHeap.remMinElt()));
+		assertTrue(HT.remMinEltTester(fullHeap,fullHeap.remMinElt()));
 	}
 	
 	@Test
 	public void removingWithEmpty() {
-		assertTrue(h1.remMinEltTester(emptyHeap,emptyHeap.remMinElt()));
+		assertTrue(HT.remMinEltTester(emptyHeap,emptyHeap.remMinElt()));
 	}
 	
 	@Test
 	public void removeBigHeap() {
-		assertTrue(h1.remMinEltTester(largeHeap,largeHeap.remMinElt()));
+		assertTrue(HT.remMinEltTester(largeHeap,largeHeap.remMinElt()));
 	}
 
 }
